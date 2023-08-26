4c0fdb2857eca2078a3a6ba9c2766a2a763651e0
AVISHKA SHAVINDA <134716496+avishka33@users.noreply.github.com>
2023-08-25 21:54:17 -0700

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,16 +1,16 @@
+//AVISHKA SHAVINDA
+
 import fs from 'fs';
 const handler = (m) => m;
 handler.all = async function(m) {
   const vn = './media/alive.mp3';
   const chat = global.db.data.chats[m.chat];
-  // const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝑇ℎ𝑒 𝑀𝑦𝑠𝑡𝑖𝑐 - 𝐵𝑜𝑡', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
-  // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
-  if (/^bot$/i.test(m.text) && !chat.isBanned) {
+  if (/^bot|.bot|.menu|menu|.alive|alive$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    // conn.sendButton(m.chat, '', wm, [['𝙼𝙴𝙽𝚄 𝙳𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂', `#menu`]], 'conversation', { sendEphemeral: true, quoted: m })
+   
     await m.reply(`*කොහොමද යාලුවා?*`);
     conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mp4', seconds: '4556', ptt: true}, {quoted: m});
-    // conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: m })
+   
   }
   return !0;
 };
