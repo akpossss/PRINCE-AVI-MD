1998da44488051c2902fb57862bc8eb6e8da809f
AVISHKA SHAVINDA <134716496+avishka33@users.noreply.github.com>
2023-08-29 04:40:56 -0700

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -5,12 +5,21 @@ const handler = (m) => m;
 handler.all = async function(m) {
   const vn = './media/alive.mp3';
   const chat = global.db.data.chats[m.chat];
-  if (/^bot|.bot|.menu|menu|.alive|alive$/i.test(m.text) && !chat.isBanned) {
+
+  if (/^.bot|alive|bot|.alive|menu|.menu$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-   
-    await m.reply(`*කොහොමද යාලුවා?*`);
+    // conn.sendButton(m.chat, '', wm, [['𝙼𝙴𝙽𝚄 𝙳𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂', `#menu`]], 'conversation', { sendEphemeral: true, quoted: m })
+    await m.reply(`  *━━━━━━━━━━━━━━*
+    *🗡PRINCE-AVI-MD🗡*
+    *━━━━━━━━━━━━━━*
+    ║➤ WHATSAPP USER BOT 
+    CREATED BY 
+    
+    ║➤ *AVISHKA SHAVINDA*
+   *━━━━━━━━━━━━━━*
+`);
     conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mp4', seconds: '4556', ptt: true}, {quoted: m});
-   
+
   }
   return !0;
 };
