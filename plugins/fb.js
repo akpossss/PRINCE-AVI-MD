bf1ace14e1271638f86831f255a3f0a1b2d4bc41
AVISHKA SHAVINDA <134716496+avishka33@users.noreply.github.com>
2023-10-11 03:24:30 -0700

    🗡AVI🗡

--- plugins/fb.js
+++ plugins/fb.js
@@ -5,19 +5,24 @@ import fbDownloader from 'fb-downloader-scrapper';
 import {facebook} from '@xct007/frieren-scraper';
 import axios from 'axios';
 const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!args[0]) throw `*𝙵𝙰𝙲𝙴𝙱𝙾𝙾𝙺, link එක දෙන්න 
+  if (!args[0]) throw `━━━━━━━━━━━━━━
+  උදාහරණය
+  ━━━━━━━━━━━━━━
   
-  Example: ${usedPrefix + command} https://fb.watch/fOTpgn6UFQ/`;
-  if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `*EXAMPLE:
-  ${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`;
+  ${usedPrefix + command} https://fb.watch//`;
+  if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `*━━━━━━━━━━━━━━ 
+  උදාහරණය;
+  ━━━━━━━━━━━━━
+  
+  ${usedPrefix + command}* https://fb.watch//`;
   try {
-    await m.reply(`*[❗] ඔයාගෙ විඩියො එක ඩව්න්ලෝඩ් වෙනො විනාඩි 2ත් 10ත් අතර කාලයක් ගතවිය හැක...*`);
+    await m.reply(`*ඔයාගෙ විඩියො එක ඩව්න්ලෝඩ් වෙනො විනාඩි 2ත් 10ත් අතර කාලයක් ගතවිය හැක......*`);
     const d2ata = await facebook.v1(args[0]);
     let r2es = '';
     if (d2ata.urls && d2ata.urls.length > 0) {
       r2es = `${d2ata.urls[0]?.hd || d2ata.urls[1]?.sd || ''}`;
     }
-    conn.sendFile(m.chat, r2es, 'error.mp4', `*🗡️AVI🗡️*`, m);
+    conn.sendFile(m.chat, r2es, 'error.mp4', `*🗡AVI🗡*`, m);
   } catch (err1) {
     try {
       const req = await igeh(args[0]);
@@ -28,31 +33,31 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
         const Jjson = await Rres.json();
         let VIDEO = Jjson.result[0];
         if (VIDEO == '' || !VIDEO || VIDEO == null) VIDEO = Jjson.result[1];
-        conn.sendFile(m.chat, VIDEO, 'error.mp4', `*🗡️AVI🗡️*`, m);
+        conn.sendFile(m.chat, VIDEO, 'error.mp4', `*🗡AVI🗡*`, m);
       } catch (err2) {
         try {
           const ress = await fg.fbdl(args[0]);
           const urll = await ress.data[0].url;
-          await conn.sendFile(m.chat, urll, 'error.mp4', '*🗡️AVI🗡️*', m);
+          await conn.sendFile(m.chat, urll, 'error.mp4', '*🗡AVI🗡*', m);
         } catch (err3) {
           try {
             const res = await fbDownloader(args[0]);
             for (const result of res.download) {
               const ur = result.url;
-              await conn.sendFile(m.chat, ur, 'error.mp4', '*🗡️AVI🗡️*', m);
+              await conn.sendFile(m.chat, ur, 'error.mp4', '*🗡AVI🗡*', m);
             }
           } catch (err4) {
             try {
               const res3 = await fetch(`https://latam-api.vercel.app/api/facebookdl?apikey=nekosmic&q=${args[0]}`);
               const json = await res3.json();
               const url3 = await json.video;
-              await conn.sendFile(m.chat, url3, 'error.mp4', '*🗡️AVI🗡️*', m);
+              await conn.sendFile(m.chat, url3, 'error.mp4', '*🗡AVI🗡*', m);
             } catch (err5) {
               try {
                 const {result} = await facebookdl(args[0]).catch(async (_) => await facebookdlv2(args[0])).catch(async (_) => await savefrom(args[0]));
-                for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*🗡️AVI🗡️*', m);
+                for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*🗡AVI🗡*', m);
               } catch (err6) {
-                throw `* 𝙴𝚁𝚁𝙾𝚁,*`;
+                throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁*`;
               }
             }
           }
@@ -61,6 +66,7 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
     }
   }
 };
+
 handler.command = /^(facebook|fb|facebookdl|fbdl|facebook2|fb2|facebookdl2|fbdl2|facebook3|fb3|facebookdl3|fbdl3|facebook4|fb4|facebookdl4|fbdl4|facebook5|fb5|facebookdl5|fbdl5)$/i;
 export default handler;
 
